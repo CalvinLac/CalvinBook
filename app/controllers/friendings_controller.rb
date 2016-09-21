8d05f9f5b3d2668c55f878b13ce891fcdad58a67
Calvin <Calvin.lacc@gmail.com>
2016-09-20 19:31:12 -0600

    Added function so that you cannot add friends you have already friended

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -2,4 +2,15 @@ class FriendingsController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @friendings = Friending.new
+    @friended_by = User.find(params[:id])
+    @friendings.friender_id = current_user.id
+    @friendings.friend_id= @friended_by.id
+    @friendings.save
+    redirect_to friendings_path
+
+  end
+
 end
